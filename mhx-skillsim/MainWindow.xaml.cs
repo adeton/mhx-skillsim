b3e22e8ad1026eacd07bf2de87d4c2b76c2b1cb3
adeton <ryohei.inozuka@gmail.com>
2016-01-08 21:05:58 +0900

    add lib 'mvvmlight' & mod namespace

--- mhx-skillsim/MainWindow.xaml.cs
+++ mhx-skillsim/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace mhx_skillsim
+namespace Adeton.Tools.MhxSkillSim
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
